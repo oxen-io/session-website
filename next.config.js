d1124d35887525b84bbf69509b4dd52e91e3e496
William Grant <willmgrant@gmail.com>
2021-09-29 14:36:26 +1000

    changed referrer policy to default value

--- next.config.js
+++ next.config.js
@@ -45,7 +45,7 @@ const securityHeaders = () => {
     },
     {
       key: 'Referrer-Policy',
-      value: 'origin-when-cross-origin',
+      value: 'strict-origin-when-cross-origin',
     },
     {
       key: 'Content-Security-Policy',
